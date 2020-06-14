eb8675f67dbe6558e0c393227314769c93357a40
Olli Lehikoinen <olli.lehikoinen@taaleri.com>
2020-06-14 15:51:57 +0300

    Started adding functionality for users

---
+++ Models/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListProject.Models
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+
+        public List<TodoListDto> Lists { get; set; }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoListProject.Database;
+using TodoListProject.Mappers;
+using TodoListProject.Models;
+
+namespace TodoListProject.Services
+{
+    public interface IUserService
+    {
+        Task CreateUser(string userName);
+        Task<TodoListDto> JoinList(string userName, Guid listId);
+    }
+
+    public class UserService : IUserService
+    {
+        public readonly TodoListContext Context;
+
+        public UserService(TodoListContext context)
+        {
+            Context = context;
+        }
+
+        public async Task CreateUser(string userName)
+        {
+            var user = new User { UserName = userName };
+
+            Context.Users.Add(user);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task<TodoListDto> JoinList(string userName, Guid listId)
+        {
+            var listToJoin = Context.TodoLists.FirstOrDefault(x => x.Id == listId);
+            var user = Context.Users.FirstOrDefault(x => x.UserName == userName);
+
+            if (listToJoin != null && user != null)
+            {
+                user.TodoLists.Add(listToJoin);
+                await Context.SaveChangesAsync();
+            }
+            else
+            {
+                return null;
+            }
+
+            return listToJoin.ToDto();
+        }
+
+
+    }
+}
